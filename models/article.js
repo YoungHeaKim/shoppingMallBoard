7d3fac1d72c7f9e41cecf2eba207302e77a9428d
younghea <dydgo9507@gmail.com>
2018-05-31 18:42:31 +0900

    쇼핑몰에 관한 테이블 작성

---
+++ models/article.js
@@ -0,0 +1,32 @@
+const timestamps = require('mongoose-timestamp');
+const paginate = require('mongoose-paginate');
+const Schema = require('mongoose').Schema;
+const articleJson = require('./json/article.json');
+const articleSchema = Schema(
+  articleJson,
+  {
+    toObject: { virtuals: true }
+  });
+
+articleSchema.plugin(timestamps);
+articleSchema.plugin(paginate);
+
+articleSchema.virtual('getDate').get(function () {
+  const date = new Date(this.createdAt);
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate()
+  }
+})
+
+articleSchema.virtual('updatedDate').get(function () {
+  const date = new Date(this.updatedAt);
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate()
+  }
+})
+
+module.exports = db.model('Article', articleSchema);
