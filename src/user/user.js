3581ada51988e1b4a0e51389cebd61add39d06a2
younghea <dydgo9507@gmail.com>
2018-05-31 18:45:28 +0900

    파일 명 변경

--- src/user/user.js
+++ src/user/user.js
@@ -1,19 +1,30 @@
 const express = require('express');
 
 const cookie = require('./user.cookie.controller');
-const register = require('./user.register.controller');
+const join = require('./user.join.controller');
 
 const app = express();
 
 const router = express.Router();
 
-// 회원가입 부분
-router.get('/register', (req, res) => {
-  res.render('login/register');
+// admin 회원가입 부분
+router.get('/admin/join', (req, res) => {
+  res.render('login/adminRegister');
 })
 
+// 파트너사 회원가입 부분
+router.get('/join', (req, res) => {
+  res.render('login/Register');
+})
+
+// admin 회원가입 저장하는 부분
+router.post('/admin/join', join.adminSignUp);
+
 // 회원가입 저장하는 부분
-router.post('/register', register.signUp);
+router.post('/join', join.signUp);
+
+// 로그인
+router.post('/cookie', cookie.cookie);
 
 // login 창
 router.get('/login', (req, res) => {
@@ -22,5 +33,16 @@ router.get('/login', (req, res) => {
 
 // 로그아웃
 router.get('/logout', cookie.cookieRemove);
+
+
+// user 수정하는 페이지
+router.get('/edit', (req, res) => {
+  res.render('login/edit');
+})
+
+// 수정하는 부분(ejs에서)
+router.post('/edit', join.edit);
+// 수정하는 부분(back)
+router.put('/edit', join.edit);
 
 module.exports = router;
