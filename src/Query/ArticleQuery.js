7bc61a44409471b6f02bbc9155193a140328d45a
younghea <dydgo9507@gmail.com>
2018-05-31 18:44:26 +0900

    게시글 부분에 대한 쿼리 작성

---
+++ src/Query/ArticleQuery.js
@@ -0,0 +1,36 @@
+const Article = require('../../models/article');
+
+module.exports = {
+  removeArticle(data) {
+    return Article.remove(data);
+  },
+  createArticle(data) {
+    return Article.create({
+      title: data.title,
+      writer: data.writer,
+      thumbnail: data.thumbnail,
+      content: data.content
+    })
+  },
+  findArticleById(data) {
+    return Article.findById(data)
+  },
+  updateTitleAndDescription(Id, data) {
+    return Article.findByIdAndUpdate(Id, {
+      title: data.title,
+      thumbnail: data.thumbnail,
+      content: data.content
+    })
+  },
+  findAllArticle(data) {
+    return Article.find(data);
+  },
+  ArticleCount(data) {
+    return Article.count({ data })
+  },
+  findArticleByTitle(data) {
+    return Article.findOne({
+      title : data
+    })
+  },
+}
