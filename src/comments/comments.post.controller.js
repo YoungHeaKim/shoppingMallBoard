c7d4cb3f88e2863c0345dd8cff75526671befcde
younghea <dydgo9507@gmail.com>
2018-06-01 18:52:55 +0900

    요청사항을 등록하는 부분 작성완료

---
+++ src/comments/comments.post.controller.js
@@ -0,0 +1,23 @@
+const query = require('../Query');
+const jwt = require('jsonwebtoken');
+
+exports.postComments = async (req, res) => {
+  // 1. 로그인되어 있는 유저의 정보를 가져옴
+  const token = req.cookies.auth;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await query.checkUserBy_id(decoded.id);
+  const CommentInfo = {
+    content: req.body.content,
+    shoppingMall_id: req.body.shoppingMall_id,
+    writer: user._id
+  }
+  const comment = await query.createComments(CommentInfo);
+  if (comment) {
+    return res.status(200).json({
+      id: comment.id,
+      content: comment.content,
+      writer: comment.writer,
+      message: "success"
+    })
+  }
+}
