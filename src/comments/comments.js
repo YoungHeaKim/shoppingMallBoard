9c36a411b9fc4f4c36c5c2ea340fa598ba3d96cf
younghea <dydgo9507@gmail.com>
2018-06-04 07:19:05 +0900

    comment에 대한 답을 작성하는 부분 작성 완료

--- src/comments/comments.js
+++ src/comments/comments.js
@@ -16,4 +16,7 @@ router.post('/shoppingmall/ajax_comment/insert', posting.postComments);
 // 댓글을 삭제하는 부분
 router.post('/shoppingmall/ajax_comment/delete', deleting.deleteCommnets);
 
+// 답변 등록하는 부분
+router.post('/shoppingmall/:_id/answer', posting.postAnswer);
+
 module.exports = router;
