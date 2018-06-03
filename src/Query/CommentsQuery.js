9b4683277c04465d76ebfeb26e736a76a0e37d86
younghea <dydgo9507@gmail.com>
2018-06-04 07:22:12 +0900

    쿼리 작성

--- src/Query/CommentsQuery.js
+++ src/Query/CommentsQuery.js
@@ -3,24 +3,39 @@ const Comments = require('../../models/comment');
 module.exports = {
   findCommentsByShoppingMall_id(id) {
     return Comments.find({
-      shoppingMall_id : id
+      shoppingMall_id: id
     });
   },
   createComments(data) {
     return Comments.create({
-      content : data.content,
-      shoppingMall_id : data.shoppingMall_id,
+      content: data.content,
+      shoppingMall_id: data.shoppingMall_id,
       writer: data.writer
     });
   },
   deleteComment(data) {
     return Comments.remove({
-      _id : data
+      _id: data
     });
   },
   deleteCommentFromArticle(data) {
     return Comments.remove({
-      shoppingMall_id : data
+      shoppingMall_id: data
+    })
+  },
+  findAllCommnets(data) {
+    return Comments.find(data)
+  },
+  createAnswer(Id, data) {
+    return Comments.update(Id, {
+      $push : {
+        answer : data
+      }
+    })
+  },
+  findCommentsBy_id(data) {
+    return Comments.findOne({
+      _id : data
     })
   }
 }
