e95d14406941ca5731d8431422cb24066077c910
younghea <dydgo9507@gmail.com>
2018-06-04 07:18:35 +0900

    comment삭제하는 부분 완성

--- src/comments/comments.delete.controller.js
+++ src/comments/comments.delete.controller.js
@@ -2,6 +2,7 @@ const query = require('../Query');
 
 exports.deleteCommnets = async (req, res) => {
   const comment = await query.deleteComment(req.body.comment_id)
+  
   if (!comment) {
     return res.status(404).json({message: "삭제 실패"})
   }
