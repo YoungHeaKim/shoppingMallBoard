3f3d0186d0d513e5705f9c9bf044d53efde8b3d5
younghea <dydgo9507@gmail.com>
2018-06-09 13:46:29 +0900

    shoppingmall_id를 변수로 저장하여 등록하는 부분 추가

--- src/comments/comments.post.controller.js
+++ src/comments/comments.post.controller.js
@@ -6,43 +6,42 @@ exports.postComments = async (req, res) => {
   const token = req.cookies.auth;
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const user = await query.checkUserBy_id(decoded.id);
+  const shoppingMall_id = req.body.shoppingMall_id;
   const CommentInfo = {
     content: req.body.content,
-    shoppingMall_id: req.body.shoppingMall_id,
+    shoppingMall_id: shoppingMall_id,
     writer: user._id
   }
   const comment = await query.createComments(CommentInfo);
+  
   if (comment) {
     return res.status(200).json({
       id: comment.id,
       content: comment.content,
       writer: comment.writer,
-      message: "success"
+      message: "success",
+      shoppingMall_id: shoppingMall_id
     })
   }
 }
 
-// 이부분 오류(ajax);
 exports.postAnswer = async (req, res) => {
   // 1. 로그인되어 있는 유저의 정보를 가져옴
   const token = req.cookies.auth;
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const user = await query.checkUserBy_id(decoded.id);
+  const comment_id = req.body.comment_id;
+  const shoppingMall_id = req.body.shoppingMall_id;
+  // 답변 부분 등록하는 부분
   const CommentInfo = {
     content : req.body.content,
     writer : user.id,
   }
-console.log(comment_id);
-
+  
   const comments = await query.createAnswer({_id : comment_id}, CommentInfo);
   
   const comment = await query.findCommentsBy_id(comments._id)
   if (comments) {
-    return res.status(200).json({
-      id: comment.id,
-      content: comment.content,
-      writer: comment.writer,
-      message: "success"
-    });
+    return res.status(200).redirect('/shoppingmall/'+shoppingMall_id);
   }
 }
